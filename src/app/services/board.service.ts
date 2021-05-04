2ceda69610e4efc12b6d64669d96bad945852360
Troy Nordine <tdnordine@gmail.com>
2021-05-04 16:12:28 -0500

    Fixed board.service.ts to make request not canned.

--- src/app/services/board.service.ts
+++ src/app/services/board.service.ts
@@ -23,53 +23,53 @@ export class BoardService {
 
   public getBoardMembers() : Observable<BoardMember[]>
   {
-    //return this.http.get<BoardMember[]>('assets/data/board.php');
+    return this.http.get<BoardMember[]>('assets/data/board.php');
 
-    return new Observable<BoardMember[]>((observer) => {
+    // return new Observable<BoardMember[]>((observer) => {
 
-      observer.next([
-        {
-          id: '1',
-          title: 'President',
-          name: 'Bruce Karnick',
-          emailaddr: 'president@hastingshawks.com',
-          termexpiration: '2019-10-31',
-          image: 'Shaun.jpg',
-          shorttitle: 'pres'
-        },
-        {
-          id: '2',
-          title: 'Vice President',
-          name: 'Troy Nordine',
-          emailaddr: 'vicepresident@hastingshawks.com',
-          termexpiration: '2019-10-31',
-          image: 'hawks-medallion.png',
-          shorttitle: 'vp'
-        },
-        {
-          id: '3',
-          title: 'Treasurer',
-          name: 'Derek Simon',
-          emailaddr: 'treasurer@hastingshawks.com',
-          termexpiration: '2020-10-31',
-          image: 'hawks-medallion.png',
-          shorttitle: 'treas'
-        },
-        {
-          id: '4',
-          title: 'Secretary',
-          name: 'Kathy Karnick',
-          emailaddr: 'secretary@hastingshawks.com',
-          termexpiration: '2019-10-31',
-          image: 'hawks-medallion.png',
-          shorttitle: 'sec'
-        }
+    //   observer.next([
+    //     {
+    //       id: '1',
+    //       title: 'President',
+    //       name: 'Bruce Karnick',
+    //       emailaddr: 'president@hastingshawks.com',
+    //       termexpiration: '2019-10-31',
+    //       image: 'Shaun.jpg',
+    //       shorttitle: 'pres'
+    //     },
+    //     {
+    //       id: '2',
+    //       title: 'Vice President',
+    //       name: 'Troy Nordine',
+    //       emailaddr: 'vicepresident@hastingshawks.com',
+    //       termexpiration: '2019-10-31',
+    //       image: 'hawks-medallion.png',
+    //       shorttitle: 'vp'
+    //     },
+    //     {
+    //       id: '3',
+    //       title: 'Treasurer',
+    //       name: 'Derek Simon',
+    //       emailaddr: 'treasurer@hastingshawks.com',
+    //       termexpiration: '2020-10-31',
+    //       image: 'hawks-medallion.png',
+    //       shorttitle: 'treas'
+    //     },
+    //     {
+    //       id: '4',
+    //       title: 'Secretary',
+    //       name: 'Kathy Karnick',
+    //       emailaddr: 'secretary@hastingshawks.com',
+    //       termexpiration: '2019-10-31',
+    //       image: 'hawks-medallion.png',
+    //       shorttitle: 'sec'
+    //     }
 
-      ]);
-      observer.complete();
+    //   ]);
+    //   observer.complete();
 
-      return {unsubscribe() {}};
-    });
+    //   return {unsubscribe() {}};
+    // });
 
   }
 }
